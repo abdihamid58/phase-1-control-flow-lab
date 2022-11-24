1fee4b6cfb15a846c3b8f2855b406e6f6a51c1fb
abdihamid <ibrahimabdy58@gmail.com>
2022-11-24 07:36:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(freeSample){
+  //i Write your code here!
+  if(freeSample<=2500){
+  if(freeSample<=400){
+    return "This one is on me!"
+  }
+   else if(freeSample>2000){
+    return `I will gladly take your thirty bucks.`
+
+   }
+  }
+  else 
+  {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  
+  return city ==='NYC' ? 'Ok, sounds good.': "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch(tip){
+ case'generous':return 'Thank you so much.';
+break;
+case'not as generous':return 'Thank you.'
+break;
+default:
+  return 'Bye.';
+}
+
 }
